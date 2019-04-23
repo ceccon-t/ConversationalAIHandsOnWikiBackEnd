e6345d31ab49e1a2aa9be4b39d61df09ba415342
Tiago Ceccon <ceccon.t@hotmail.com>
2019-04-22 23:07:40 -0300

    Add support for information about bands

--- service.py
+++ service.py
@@ -8,6 +8,15 @@ def _reply(type="text", content=""):
     }
 
 
+def _get_message_to(queried):
+    message = ""
+    current_response = knowledge.get_wikipedia_intro(queried)
+    if current_response == "":
+        current_response = "Hmmm... I do not know anything about " + queried + ". Are you sure the spelling is correct?"
+    message += current_response
+    return message
+
+
 def process_info(request):
     replies = []
     for intent in request.get_intents_present():
@@ -19,11 +28,10 @@ def process_info(request):
 def reply_information(request):
     replies = []
     for person in request.get_all_values_for_entity('person'):
-        message = ""
-        current_response = knowledge.get_wikipedia_intro(person)
-        if current_response == "":
-            current_response = "Hmmm... I do not know anything about " + person + ". Are you sure the spelling is correct?"
-        message += current_response
+        message = _get_message_to(person)
+        replies.append(_reply("text", message))
+    for band in request.get_all_values_for_entity('band'):
+        message = _get_message_to(band)
         replies.append(_reply("text", message))
     return replies
 
